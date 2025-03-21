3cc6a2b08353a28a36667561c4061d420e06fd7c
Sam Li <samli@cloudeasy.net>
2025-03-21 14:59:22 +0800

    feat: add page and layout transition

--- app.vue
+++ app.vue
@@ -12,6 +12,28 @@
   nuxtApp.vueApp.use(VueDragScroller)
 </script>
 
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.4s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  filter: grayscale(1);
+}
+
+</style>
+
 <style lang="scss">
 @use "~/assets/scss/main.scss";
 
