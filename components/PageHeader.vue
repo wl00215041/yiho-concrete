284b75cbe6f79618a7dd9eb632dfe200caa9ab98
Sam Li <samli@cloudeasy.net>
2025-03-22 09:07:30 +0800

    feat: add logo link

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex p-6 pad:py-8 pad:px-6 desktop:py-[26px] desktop:px-11 justify-between items-center">
-    <SvgoPageLogo filled class="text-[221px] !h-[45px] !mb-0"></SvgoPageLogo>
+    <SvgoPageLogo @click="$router.push({ path: '/' })" filled class="text-[221px] !h-[45px] !mb-0 cursor-pointer"></SvgoPageLogo>
     <navbar></navbar>
   </div>
 </template>
