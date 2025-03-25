8fc44a5c3236216410c16015bc6a48de0b113caa
Sam Li <samli@cloudeasy.net>
2025-03-25 08:41:36 +0800

    chore: update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'git@github.com:wl00215041/yiho-concrete.git',
       path : '/home/yiho-concrete/htdocs/yiho-concrete.cloudeasy.net',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install -g pnpm && pnpm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'PATH=/home/yiho-concrete/.nvm/versions/node/v20.19.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/snap/bin npm install -g pnpm && pnpm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
