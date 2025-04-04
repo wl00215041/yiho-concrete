c111d0b06cbf2217fb16d3c8ec0c956f24c842e7
Sam Li <samli@cloudeasy.net>
2025-04-04 17:31:46 +0800

    chore: change images

--- components/Achievement/ShowCase.vue
+++ components/Achievement/ShowCase.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-between">
-    <img class="mb-2 desktop:mb-6 rounded-[10px]" :src="image" alt="acheiments">
+    <img class="mb-2 desktop:mb-6 rounded-[10px] h-[165px] pad:h-[153px] desktop:h-[260px] object-cover" :src="image" alt="acheiments">
     <div class="flex items-center gap-3 cursor-pointer">
       <SvgoArrowRight class="text-2xl desktop:text-[2rem] !mb-0 text-[#777777]"></SvgoArrowRight>
       <span class="text-base desktop:text-2xl text-[#333333]">{{ title }}</span>
