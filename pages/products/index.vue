88c3ce984752085dc4b64fdab9213ebd0449522b
Sam Li <samli@cloudeasy.net>
2025-04-04 16:16:26 +0800

    feat: adjust certification section layout

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -15,10 +15,10 @@
       icon-color="#0075C2">
       <SvgoQualityIcon filled class="absolute !w-[100px] !h-[60px] desktop:!w-[186px] desktop:!h-[107px] right-[24px] desktop:right-[30px] top-0 z-10 -translate-y-1/2"></SvgoQualityIcon>
       <SvgoQualityIcon filled class="absolute !w-[100px] !h-[60px] desktop:!w-[186px] desktop:!h-[107px] left-[24px] desktop:left-[30px] bottom-0 z-10 translate-y-1/2 rotate-180"></SvgoQualityIcon>
-      <div class="flex flex-col pad:justify-center pad:flex-row gap-6 desktop:gap-[60px] pb-10">
-        <div class="w-full max-w-[446px]" v-if="displayCertifications.standard.length">
+      <div class="flex flex-col items-center pad:justify-center pad:flex-row gap-6 desktop:gap-[60px] pb-10">
+        <div class="w-full max-w-[446px]" :class="{ 'pad:!max-w-3xl': isOnlyStandard }" v-if="displayCertifications.standard.length">
           <div class=" text-white text-center bg-[#0075C2] text-lg rounded-[20px] py-[10px]">國家標準與法規</div>
-          <ul class="list-disc ">
+          <ul class="list-disc" :class="{ 'pad:!grid pad:!grid-cols-2 pad:!gap-10': isOnlyStandard }">
             <li v-for="standard in displayCertifications.standard" class="flex justify-between items-center px-[30px] py-[22px] border-b border-dashed border-[#0075C2]">
               <span>{{ standard.name }}</span>
               <a v-if="standard.file" :href="`/files/certifications/${standard.file}`" target="_blank">
@@ -27,9 +27,9 @@
             </li>
           </ul>
         </div>
-        <div class="w-full  max-w-[446px]" v-if="displayCertifications.certification.length">
+        <div class="w-full  max-w-[446px]" :class="{ 'pad:!max-w-3xl': isOnlyCertification }" v-if="displayCertifications.certification.length">
           <div class="w-full text-white text-center bg-[#0075C2] text-lg rounded-[20px] py-[10px]">國際認證</div>
-          <ul class="list-disc">
+          <ul class="list-disc" :class="{ 'pad:!grid pad:!grid-cols-2 pad:!gap-10': isOnlyCertification }">
             <li v-for="certification in displayCertifications.certification" class="flex justify-between px-[30px] items-center py-[22px] border-b border-dashed border-[#0075C2]">
               <span>{{ certification.name }}</span>
               <a v-if="certification.file" :href="`/files/certifications/${certification.file}`" target="_blank">
@@ -96,6 +96,13 @@ onMounted(() => {
       element.scrollIntoView({ behavior: 'smooth' })
     }
   }
+})
+
+const isOnlyStandard = computed(() => {
+  return displayCertifications.value.standard.length && !displayCertifications.value.certification.length
+})
+const isOnlyCertification = computed(() => {
+  return !displayCertifications.value.standard.length && displayCertifications.value.certification.length
 })
 
 
