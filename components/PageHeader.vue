63ae2d6bc03001e434003e10505938622beb6739
Sam Li <samli@cloudeasy.net>
2025-04-04 18:14:40 +0800

    fix: sticky header z-index

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex sticky z-10 top-0 p-6 pad:py-8 pad:px-6 desktop:py-[26px] desktop:px-11 justify-between items-center bg-white">
+  <div class="flex sticky z-20 top-0 p-6 pad:py-8 pad:px-6 desktop:py-[26px] desktop:px-11 justify-between items-center bg-white">
     <SvgoPageLogo @click="$router.push({ path: '/' })" filled class="text-[221px] !h-[45px] !mb-0 cursor-pointer"></SvgoPageLogo>
     <navbar></navbar>
   </div>
