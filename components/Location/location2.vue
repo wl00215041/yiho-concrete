584207cb1a72e1ef05179fce459900f47c87ccdc
Sam Li <samli@cloudeasy.net>
2025-04-04 13:28:32 +0800

    feat: update location map

--- components/Location/location2.vue
+++ components/Location/location2.vue
@@ -16,7 +16,7 @@
       <span class="font-bold mr-4 text-[#333333]">地址</span>
       <span class="text-[#585858]">苗栗縣竹南鎮港墘里港仔墘3鄰29之12號</span>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.002890365105!2d121.56196397537656!3d25.033975977816482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abb6da80a7ad%3A0xdfbc764cc6880ac8!2zR29vZ2xlIOWPsOeBow!5e0!3m2!1szh-TW!2stw!4v1743640408482!5m2!1szh-TW!2stw" width="100%" height="400" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3625.648639356094!2d120.856931!3d24.6702197!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3469b25900000001%3A0xbed76f59b990e803!2z5q-F5ZKM5a-m5qWt5pyJ6ZmQ5YWs5Y-4!5e0!3m2!1szh-TW!2stw!4v1743744318986!5m2!1szh-TW!2stw" width="100%" height="400" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     <div class=" border border-dashed border-[#BABABA] my-5"></div>
     <div class="flex items-center px-6 mb-6">
       <SvgoCapacity filled class="text-2xl mr-3"></SvgoCapacity>
--- components/Location/location3.vue
+++ components/Location/location3.vue
@@ -15,7 +15,7 @@
       <span class="font-bold mr-4 text-[#333333]">地址</span>
       <span class="text-[#585858]">新竹縣寶山鄉雙溪村1鄰三峰路二段385、387號</span>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.002890365105!2d121.56196397537656!3d25.033975977816482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abb6da80a7ad%3A0xdfbc764cc6880ac8!2zR29vZ2xlIOWPsOeBow!5e0!3m2!1szh-TW!2stw!4v1743640408482!5m2!1szh-TW!2stw" width="100%" height="400" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.0713668575117!2d120.9985737!3d24.758742100000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346849f20255086d%3A0x252b52fa9db7925!2z5q-F5ZKM5a-m5qWt5pyJ6ZmQ5YWs5Y-4!5e0!3m2!1szh-TW!2stw!4v1743744345708!5m2!1szh-TW!2stw" width="100%" height="400" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     <div class=" border border-dashed border-[#BABABA] my-5"></div>
     <div class="flex items-center px-6 mb-6">
       <SvgoCapacity filled class="text-2xl mr-3"></SvgoCapacity>
