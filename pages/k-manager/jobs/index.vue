cae07768f748ae12526bf54b0ff3c393d0618399
Sam Li <samli@cloudeasy.net>
2025-04-04 16:05:27 +0800

    feat: add job delete confirm dialog

--- pages/k-manager/jobs/index.vue
+++ pages/k-manager/jobs/index.vue
@@ -10,17 +10,19 @@
                 <button @click="open" class="min-w-[110px] py-3 rounded bg-[#0075C2] text-white">新增</button>
               </template>
             </ManagerJobsAddJobModel>
-          <button @click="onDeleteJobs" class="min-w-[110px] py-3 rounded bg-[#E8382F] text-white">刪除</button>
+          <button @click="open" class="min-w-[110px] py-3 rounded bg-[#E8382F] text-white">刪除</button>
         </div>
       </div>
       <ManagerTable :columns="columns" :records="jobList || []" :selectable="true" @selectionChange="onSelectionChange">
 
       </ManagerTable>
+      <ManagerConfirmDialog v-model="opened" @confirm="onConfirm"></ManagerConfirmDialog>
     </div>
   </ManagerPage>
 </template>
 <script setup lang="ts">
 import { useDayjs } from '#dayjs'
+import { useConfirm } from '~/hooks/useConfirm';
 
 definePageMeta({
   layout: 'manager',
@@ -39,7 +41,12 @@ const columns = [
 const isAddModalOpened = ref(false)
 const selectedJob = ref<number[]>([])
 
-
+const { opened, open, onConfirm } = useConfirm({
+  confirm: async () => {
+    await $trpcClient.manager.batchDeleteJobs.mutate(selectedJob.value)
+    refresh()
+  }
+})
 
 const { $trpcClient } = useNuxtApp()
 
@@ -63,13 +70,5 @@ const onAdd = async (job: any) => {
 const onSelectionChange = (selectedIds: number[]) => {
   selectedJob.value = selectedIds
 };
-
-const onDeleteJobs = async () => {
-  const confirmDelete = confirm('確定要刪除嗎？')
-  if (!confirmDelete) return
-  console.log(selectedJob.value)
-  await $trpcClient.manager.batchDeleteJobs.mutate(selectedJob.value)
-  refresh()
-}
 
 </script>
