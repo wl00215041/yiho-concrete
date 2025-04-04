b35e720f25d6ed754d3906c363cc233ac7d0f4c0
Sam Li <samli@cloudeasy.net>
2025-04-04 09:29:18 +0800

    fix: footer z-index

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative overflow-hidden pt-16 pad:pt-0 flex flex-col pad:flex-row items-center justify-center pad:justify-between footer h-[278px] pad:h-[407px] desktop:h-[299px] px-[50px] mt-12 z-[-1] bg-[#333333] rounded-tl-[60px]">
+    class="relative overflow-hidden pt-16 pad:pt-0 flex flex-col pad:flex-row items-center justify-center pad:justify-between footer h-[278px] pad:h-[407px] desktop:h-[299px] px-[50px] mt-12 bg-[#333333] rounded-tl-[60px]">
     <div class="flex flex-col items-center">
       <SvgoFullLogo filled class="text-[221px] !h-[45px]"></SvgoFullLogo>
       <div class="text-xl text-white text-opacity-80">© 2025 YIHO Ltd. All rights reserved.</div>
