4b92b808163396a546ccb6c0cb139288bdc989f8
Sam Li <samli@cloudeasy.net>
2025-04-04 16:39:24 +0800

    feat: adjust certification layout

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <PageBanner image="/images/product-banner.png" title="產品資訊" sub-title="Products" sub-title-color="#0075C2"></PageBanner>
+    <PageBanner image="/images/product-banner.png" title="產品資訊" sub-title="Products" sub-title-color="#0075C2">
+    </PageBanner>
     <PageSection class="mb-[60px]" title="產品類別" icon-color="#0075C2">
       <div class="grid grid-cols-1 pad:grid-cols-2 gap-8">
-        <ProductInfo :title="product.title" @click="$router.push({ name: 'products-id', params: { id: product.id }  })" :image="product.imageUrl" v-for="product in products" :key="product.id">
+        <ProductInfo :title="product.title" @click="$router.push({ name: 'products-id', params: { id: product.id } })"
+          :image="product.imageUrl" v-for="product in products" :key="product.id">
           <pre class="text-wrap">
 {{ product.description }}
           </pre>
@@ -11,34 +13,46 @@
       </div>
       <div></div>
     </PageSection>
-    <PageSection title="品質認證" id="certification" class="py-7 bg-[#0075C2] bg-opacity-10 pad:mx-[30px] pad:rounded-[30px] relative mb-[60px]"
-      icon-color="#0075C2">
-      <SvgoQualityIcon filled class="absolute !w-[100px] !h-[60px] desktop:!w-[186px] desktop:!h-[107px] right-[24px] desktop:right-[30px] top-0 z-10 -translate-y-1/2"></SvgoQualityIcon>
-      <SvgoQualityIcon filled class="absolute !w-[100px] !h-[60px] desktop:!w-[186px] desktop:!h-[107px] left-[24px] desktop:left-[30px] bottom-0 z-10 translate-y-1/2 rotate-180"></SvgoQualityIcon>
-      <div class="flex flex-col items-center pad:justify-center pad:flex-row gap-6 desktop:gap-[60px] pb-10">
-        <div class="w-full max-w-[446px]" :class="{ 'pad:!max-w-3xl': isOnlyStandard }" v-if="displayCertifications.standard.length">
-          <div class=" text-white text-center bg-[#0075C2] text-lg rounded-[20px] py-[10px]">國家標準與法規</div>
-          <ul class="list-disc" :class="{ 'pad:!grid pad:!grid-cols-2 pad:!gap-x-10': isOnlyStandard }">
-            <li v-for="standard in displayCertifications.standard" class="flex justify-between items-center px-[30px] py-[22px] border-b border-dashed border-[#0075C2]">
-              <span>{{ standard.name }}</span>
-              <a v-if="standard.file" :href="`/files/certifications/${standard.file}`" target="_blank">
-                <SvgoDocument filled></SvgoDocument>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="w-full  max-w-[446px]" :class="{ 'pad:!max-w-3xl': isOnlyCertification }" v-if="displayCertifications.certification.length">
-          <div class="w-full text-white text-center bg-[#0075C2] text-lg rounded-[20px] py-[10px]">國際認證</div>
-          <ul class="list-disc" :class="{ 'pad:!grid pad:!grid-cols-2 pad:!gap-x-10': isOnlyCertification }">
-            <li v-for="certification in displayCertifications.certification" class="flex justify-between px-[30px] items-center py-[22px] border-b border-dashed border-[#0075C2]">
-              <span>{{ certification.name }}</span>
-              <a v-if="certification.file" :href="`/files/certifications/${certification.file}`" target="_blank">
-                <SvgoDocument filled></SvgoDocument>
-              </a>
-            </li>
-          </ul>
+    <PageSection title="品質認證" id="certification"
+      class="py-7 bg-[#0075C2] bg-opacity-10 pad:mx-[30px] pad:rounded-[30px] relative mb-[60px]" icon-color="#0075C2">
+      <SvgoQualityIcon filled
+        class="absolute !w-[100px] !h-[60px] desktop:!w-[186px] desktop:!h-[107px] right-[24px] desktop:right-[30px] top-0 z-10 -translate-y-1/2">
+      </SvgoQualityIcon>
+      <SvgoQualityIcon filled
+        class="absolute !w-[100px] !h-[60px] desktop:!w-[186px] desktop:!h-[107px] left-[24px] desktop:left-[30px] bottom-0 z-10 translate-y-1/2 rotate-180">
+      </SvgoQualityIcon>
+      <div class="flex justify-center">
+        <div
+          class="flex flex-col pad:justify-center w-[calc(70%+220px)] pad:flex-row gap-[60px] desktop:gap-[60px] pb-10"
+          :class="{ 'items-center': isOnlyStandard || isOnlyCertification }">
+          <div class="flex-1" v-if="displayCertifications.standard.length">
+            <div class=" text-white text-center bg-[#0075C2] text-lg rounded-[20px] py-[10px]">國家標準與法規</div>
+            <ul class="list-disc" :class="{ 'pad:!grid pad:!grid-cols-2 pad:!gap-x-10': isOnlyStandard }">
+              <li v-for="standard in displayCertifications.standard"
+                class="flex justify-between items-center px-[30px] py-[22px] border-b border-dashed border-[#0075C2]">
+                <span>{{ standard.name }}</span>
+                <a v-if="standard.file" :href="`/files/certifications/${standard.file}`" target="_blank">
+                  <SvgoDocument filled></SvgoDocument>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="flex-1" v-if="displayCertifications.certification.length">
+            <div class="w-full text-white text-center bg-[#0075C2] text-lg rounded-[20px] py-[10px]"
+              :class="{ 'items-center': isOnlyStandard || isOnlyCertification }">國際認證</div>
+            <ul class="list-disc" :class="{ 'pad:!grid pad:!grid-cols-2 pad:!gap-x-10': isOnlyCertification }">
+              <li v-for="certification in displayCertifications.certification"
+                class="flex justify-between px-[30px] items-center py-[22px] border-b border-dashed border-[#0075C2]">
+                <span>{{ certification.name }}</span>
+                <a v-if="certification.file" :href="`/files/certifications/${certification.file}`" target="_blank">
+                  <SvgoDocument filled></SvgoDocument>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+
     </PageSection>
     <!-- <PageSection title="炭足跡" icon-color="#0075C2">
         <div class="relative flex flex-col justify-center items-center ">
@@ -86,7 +100,7 @@ const displayCertifications = computed(() => {
     standard: certifications.value?.filter((certification) => certification.type === 'standard') || [],
     certification: certifications.value?.filter((certification) => certification.type === 'certification') || [],
   }
-  return list 
+  return list
 })
 
 onMounted(() => {
